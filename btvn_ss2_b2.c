924a301ea112cfbd294955ffb1011784ca770f2a
haphuchung <haphuchung2006@gmail.com>
2024-11-20 20:14:06 +0700

    Add files via upload

---
+++ btvn_ss2_b2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+	// khai bao va khoi tao hai bien so nguyen
+	int number1 = 1; // bien so nguyen dau tien 
+	int number2 = 2; // bien so nguyen thu hai 
+	
+	// khai bao mot bien de luu ket qua tinh tong 
+	int sum = number1 + number2; //tinh tong cua number1 va number2 
+	
+	// in ra ket qua 
+	printf ("tong cua %d va %d la: %d/n", number1, number2, sum );
+
+	return 0;
+}
